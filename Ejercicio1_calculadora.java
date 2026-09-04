0b5c06ddf1cbfa2f1197ff1dd1be5b01cfb0aa5e
Williamvilla25 <williamvilla727@gmail.com>
2026-09-04 17:58:01 -0500

    Creando variables y scanner para digitacion de numeros a operar

--- Ejercicio1_calculadora.java
+++ Ejercicio1_calculadora.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Ejercicio1_calculadora {
     public static void main(String[] args) {
-        
+        float Numero1 = 0;
+        float Numero2 = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        Numero1 = scanner.nextFloat();
+        System.out.print("Ingrese el segundo número: ");
+        Numero2 = scanner.nextFloat();
     }
 }
