9de249306ec5aa289ab6b5482c7af2fd527700ce
Williamvilla25 <williamvilla727@gmail.com>
2026-09-04 17:54:02 -0500

    Creando operadores numericos

--- Operaciones.java
+++ Operaciones.java
@@ -8,7 +8,24 @@ public class Operaciones {
     }
 
     public Operaciones(float Numero1, float Numero2){
-        this.Numero1 = Numero1;
-        this.Numero2 = Numero2;
+        Numero1 = Numero1;
+        Numero2 = Numero2;
     }
+
+    public float suma(){
+        return Numero1 + Numero2;
+    }
+
+    public float resta(){
+        return Numero1 - Numero2;
+    }
+
+    public float multiplicacion(){
+        return Numero1 * Numero2;
+    }
+
+    public float division(){
+        return Numero1 / Numero2;
+    }
+
 }
