babe6d09e7a0815d0cd46dc49beb0fe68b46f255
Williamvilla25 <williamvilla727@gmail.com>
2026-09-04 19:05:48 -0500

    creando un modelo que devuelva la cadena de texto

--- InversorCadena.java
+++ InversorCadena.java
@@ -5,5 +5,12 @@ public class InversorCadena {
         this.texto = texto;
     }
 
+    public String invertir(){
+        String resulto = "";
+        for (int i = texto.length() - 1; i >= 0; i--) {
+            resulto += texto.charAt(i);
+        }     
+        return resulto;
+    }
 }
 
